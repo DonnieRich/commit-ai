f15d9c9300bde29336c1781dc5f09823911d354e
Donnie <donniepask@gmail.com>
2023-04-18 14:45:27 +0200

    Added try-catch in activate method

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,30 @@ import * as vscode from 'vscode';
 import { CommitAi } from './commitAi/CommitAi';
 
 
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
-    const openAi = new CommitAi(context);
-    openAi.checkApiKey();
+    try {
+        const openAi = new CommitAi(context);
+        await openAi.checkApiKey();
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('commit-ai.setOpenAiApiKey', () => {
-            openAi.setApiKey();
-        }));
+        context.subscriptions.push(
+            vscode.commands.registerCommand('commit-ai.setOpenAiApiKey', async () => {
+                await openAi.writeApiKey();
+            }));
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('commit-ai.clearOpenAiApiKey', () => {
-            openAi.clearApiKey();
-        }));
+        context.subscriptions.push(
+            vscode.commands.registerCommand('commit-ai.clearOpenAiApiKey', async () => {
+                await openAi.clearApiKey();
+            }));
 
-    context.subscriptions.push(
-        vscode.commands.registerCommand('commit-ai.requestCommitSuggestion', () => {
-            openAi.apiRequest();
-        }));
+        context.subscriptions.push(
+            vscode.commands.registerCommand('commit-ai.requestCommitSuggestion', async () => {
+                await openAi.apiRequest();
+            }));
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            vscode.window.showErrorMessage(error.message);
+        }
+    }
 
-    return context;
 }
